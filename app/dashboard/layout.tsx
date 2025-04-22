3da23ec2020a6576b2766252cc78811cecc9a732
Your Name <you@example.com>
2025-04-22 14:12:31 +0700

    update layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,19 +1,19 @@
-// // app/layout.tsx
-// import '/globals.css';
-// import Navbar from "../components/Navbar";
+// app/layout.tsx
+import '/globals.css';
 
-// export const metadata = {
-//   title: 'My App',
-//   description: 'Deskripsi aplikasi',
-// };
 
-// export default function RootLayout({ children }: { children: React.ReactNode }) {
-//   return (
-//     <html lang="id">
-//       <body>
-//         <Navbar />
-//         <main>{children}</main>
-//       </body>
-//     </html>
-//   // );
-// }
+export const metadata = {
+  title: 'My App',
+  description: 'Deskripsi aplikasi',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="id">
+      <body>
+        
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
